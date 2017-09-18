b7e7db6f2ac4410d9cf10e1e50011e3f57b650a5
Charlie <Charlie@WirelessPersoneel-143169156008.uantwerpen.be>
2017-09-18 12:14:36 +0200

    remove url links

--- R/Winedata.R
+++ R/Winedata.R
@@ -13,7 +13,7 @@
 #' @references  Larsen et al. (2006) An exploratory chemometric study of 1H-NMR spectra of table wines. J.Chemom. 20 (2006) 198-208
 #' (\href{http://onlinelibrary.wiley.com/doi/10.1002/cem.991/full}{Wiley Online Library})
 #'
-#' @source \href{http://www.models.life.ku.dk/datasets}{University of Copenhagen, Dept. of Food Science, Quality & Technology}
+#' @source University of Copenhagen, Dept. of Food Science, Quality & Technology. Available at 'models.life.ku.dk/datasets'
 #'
 #' @examples
 #' data(Winedata)
