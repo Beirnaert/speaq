efeffbe35ca07ab400720352a09acf4c3ccb489a
Charlie <Charlie@WirelessPersoneel-143169156008.uantwerpen.be>
2017-09-19 10:13:18 +0200

    decreased subset data size to limit examples overhead

--- R/GetWinedata.subset.R
+++ R/GetWinedata.subset.R
@@ -2,7 +2,7 @@
 #'
 #' This functions extracts a small part of the Winedata to be used in code testing and code examples
 #' 
-#' @return list of 4: spectra, ppm values, color and origin.
+#' @return list of 3: spectra, ppm values, color and origin.
 #'
 #' @author Charlie Beirnaert, \email{charlie.beirnaert@@uantwerpen.be}
 #'
@@ -15,9 +15,9 @@
 #' @export
 #' 
 GetWinedata.subset <- function(){
-    Spectra <- speaq::Winedata$spectra[1:4, 1001:2000]
+    Spectra <- speaq::Winedata$spectra[1:3, 1001:2000]
     PPM.vector <- speaq::Winedata$ppm[1001:2000]
-    wine.color <- as.character(speaq::Winedata$wine.color[1:4])
-    wine.origin <- as.character(speaq::Winedata$origin[1:4])
+    wine.color <- as.character(speaq::Winedata$wine.color[1:3])
+    wine.origin <- as.character(speaq::Winedata$origin[1:3])
     return(list(Spectra = Spectra, PPM = PPM.vector, Color = wine.color, Origin = wine.origin))
 }
