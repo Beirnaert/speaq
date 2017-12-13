6c2bf7920444fccf132504d5715c363ba3266b0c
Charlie Beirnaert <charlie.beirnaert@uantwerpen.be>
2017-12-13 12:53:38 +0100

    added simulated dataset analysis

---
+++ speaq2_Extra/SimulatedDataSet.R
@@ -0,0 +1,15 @@
+#' Simulated Case vs Control Dataset
+#'
+#' Simulated dataset constructed form 2 base spectra.
+#'
+#' @docType data
+#'
+#'
+#' @format A list with the spectra, ppm vector, group vector, the 2 base spectra from which the database is constructed, information to construct the dataset (mixingFactor, amplitudeFactor & NoiseMatrix)
+#'
+#' @keywords datasets
+#'
+#' @references  Beirnaert et al. (unpublished)  speaq 2.0: a complete workflow for high-throughput 1D NMR spectra processing and quantification.
+#' (\href{https://www.biorxiv.org/content/early/2017/06/06/138503}{DOI: 10.1101/138503})
+#'
+"SimulatedDataSet"
